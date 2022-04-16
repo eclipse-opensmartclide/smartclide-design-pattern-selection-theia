625ddf10b000ed3778f687d0d3d6e38811570f5c
Evangelia Papagiannaki <evapapag2000@gmail.com>
2022-04-16 16:00:56 +0300

    New changes

--- smartclide-design-pattern-selection-theia/src/node/hello-backend-service.ts
+++ smartclide-design-pattern-selection-theia/src/node/hello-backend-service.ts
@@ -28,25 +28,14 @@ export class HelloBackendServiceImpl implements HelloBackendService {
     }
 
     async sayHelloTo(url: string): Promise<string[]> {
-        //string manipulation to get the right form of url string
-        var lastL = url.indexOf("/#/");
-        var rootUri;
-        if(url.match("\/#\/.:\/")){
-            rootUri = url.substr(lastL+3);
-        }
-        else{
-            rootUri = url.substr(lastL+2);
-        }
-        console.log(1);
-        //search for every file name in textbox values
-        //index=-1 if not found
-        this.ThroughDirectory(rootUri);
+        this.ThroughDirectory(url);
         var res= HelloBackendServiceImpl.Files;
         HelloBackendServiceImpl.array = res;
         for (let i=0; i<res.length; i++){
-            let lastW = res[i].lastIndexOf("/");
+            let lastW = res[i].lastIndexOf("");
             let file = res[i].substr(lastW+1);
             file = file.substr(0, file.indexOf("."));
+            console.log(file);
             res[i] = file;  
         }
         
@@ -54,22 +43,14 @@ export class HelloBackendServiceImpl implements HelloBackendService {
     }
 
     async getMethods(url: string, fileName: string): Promise<string[]>{
-        var lastL = url.indexOf("/#/");
-        var rootUri;
-        if(url.match("\/#\/.:\/")){
-            rootUri = url.substr(lastL+3);
-        }
-        else{
-            rootUri = url.substr(lastL+2);
-        }
         var fs = require("fs");
         let lO = {label: []};
         try {
-            const data = fs.readFileSync(rootUri+"/src/"+ fileName +".java", 'utf8')
+            const data = fs.readFileSync(url+"/src/"+ fileName +".java", 'utf8')
             const regex = new RegExp(/(?:(?:public|private|protected|static|final|native|synchronized|abstract|transient)+\s+)+[$_\w<>\[\]\s]*\s+[\$_\w]+\([^\)]*\)?\s*/gm);
             const array = [...data.matchAll(regex)];
             for(var i = 0; i<array.length; i++){
-                var firstString = (array[i].toString()).split('(');//?
+                var firstString = (array[i].toString()).split('(');
                 var secondString = (firstString[0].toString()).split(/\s+/);
                 var item = secondString[secondString.length-1];
                 this.fillPromise(lO, item); 
