b874e0500572f2b3e812a1fdc7e7183d65862a3b
Evangelia Papagiannaki <evapapag2000@gmail.com>
2022-04-20 23:29:25 +0300

    Added Mediator branch

--- smartclide-design-pattern-selection-theia/src/browser/BehavioralPatternsWizard.tsx
+++ smartclide-design-pattern-selection-theia/src/browser/BehavioralPatternsWizard.tsx
@@ -7,8 +7,8 @@ import { HelloBackendService } from '../common/protocol';
 /*interface Textfield{
 	ident: number;
 	value: string;
-};
-*/
+};*/
+
 
 export class BehavioralPatterns{
 
@@ -28,29 +28,69 @@ export class BehavioralPatterns{
 				radio311.addEventListener('click', async (e: Event) =>{
 					divCont2.innerHTML = "";
 					let divCont3 = document.createElement('div');
-					BehavioralPatterns.functions.textfieldQuestion('<br> Insert the name of the Mediator, the interface that declares the connection method<br>', 1, 'text', 'Mediator name', 'txtboxMediator', 'infoField', 'buttonNext', divCont2);
-					let buttonNext = document.getElementById('buttonNext') as HTMLButtonElement;
-                    buttonNext.addEventListener('click', async (e: Event) =>{
+					BehavioralPatterns.functions.radioQuestion('<br>Is the receiver part of a comple component, whose internal structure you want to hide? <br>', 'Yes', 'No', 'radio3111', 'radio3112', divCont2);
+					let radio3111 = document.getElementById("radio3111") as HTMLInputElement;
+					radio3111.addEventListener('click', async(e: Event) =>{
 						divCont3.innerHTML = "";
 						let divCont4 = document.createElement('div');
-						BehavioralPatterns.functions.textfieldQuestion('<br> How many Concrete Mediators(classes that implement the connection in a different way) exist? <br>', 1, 'number', '1', 'subcategoriesNum', '', 'buttonNext1', divCont3);
-						let buttonNext1 = document.getElementById('buttonNext1') as HTMLButtonElement;
-						buttonNext1.addEventListener('click', async (e: Event) =>{
+						BehavioralPatterns.functions.textfieldQuestion('<br> Insert the name of the Mediator, the interface that declares the connection method<br>', 1, 'text', 'Mediator name', 'txtboxMediator', 'infoField', 'buttonNext', divCont3);
+						let buttonNext = document.getElementById('buttonNext') as HTMLButtonElement;
+						buttonNext.addEventListener('click', async (e: Event) =>{
 							divCont4.innerHTML = "";
-                        });
-                        divCont3.append(divCont4);
-                    }); 
+							let divCont5 = document.createElement('div');
+							BehavioralPatterns.functions.textfieldQuestion('<br>How many Concrete Mediators(classes that implement the connection in a diffirent way) exist? <br>',  1, 'number', '1', 'numOfConcreteMediators', '', 'buttonNext1', divCont4);;
+							let buttonNext1 = document.getElementById('buttonNext1') as HTMLButtonElement;
+							buttonNext1.addEventListener('click', async (e: Event) =>{
+								divCont5.innerHTML = "";
+								let divCont6 = document.createElement('div');
+								BehavioralPatterns.functions.textfieldQuestion("<br> Insert the names of the Concrete Mediators <br>", parseInt((document.getElementById('numOfConcreteMediators') as HTMLInputElement).value), 'text', "Subclass's name ", 'txtboxConcreteImplementation', 'infoField', 'buttonNext2', divCont5);
+								let buttonNext2 = document.getElementById('buttonNext2') as HTMLButtonElement;
+								buttonNext2.addEventListener('click', async (e: Event) =>{
+									divCont6.innerHTML = "";
+									BehavioralPatterns.functions.textfieldQuestion('<br>How many Components(classes that need to be connected) exist? <br>',  1, 'number', '1', 'numOfConmponents', '', 'buttonNext3', divCont4);;
+									let buttonNext3 = document.getElementById('buttonNext3') as HTMLButtonElement;
+									buttonNext3.addEventListener('click', async (e: Event) =>{
+										divCont5.innerHTML = "";
+										let divCont6 = document.createElement('div');
+										BehavioralPatterns.functions.textfieldQuestion("<br> Insert the names of the Concrete Components <br>", parseInt((document.getElementById('numOfConmponents') as HTMLInputElement).value), 'text', "Subclass's name ", 'txtboxConcreteImplementation', 'infoField', 'buttonNext4', divCont5);
+										let buttonNext4 = document.getElementById('buttonNext4') as HTMLButtonElement;
+										buttonNext4.addEventListener('click', async (e: Event) =>{
+											divCont6.innerHTML = "";
+											let divCont7 = document.createElement('div');
+											BehavioralPatterns.functions.createLabel('<br> <b>Mediator Pattern</b>  ', '', divCont7);
+											BehavioralPatterns.functions.createButton('Get Code', 'getcodeBridgePattern', divCont7);
+											let buttonCodeBP = document.getElementById('getcodeBridgePattern') as HTMLButtonElement;
+											buttonCodeBP.addEventListener('click', async (e: Event) =>{
+											});
+											divCont6.append(divCont7);
+										});
+										
+									});
+									
+								});
+								divCont5.append(divCont6);
+							});
+							divCont4.appendChild(divCont5);
+						});
+						divCont3.appendChild(divCont4);
+					});
+					let radio3112 = document.getElementById("radio3112") as HTMLInputElement;
+					radio3112.addEventListener('click', async(e: Event) =>{
+						
+					});
                     divCont2.appendChild(divCont3);
                 });
                 let radio312= document.getElementById('radio312') as HTMLInputElement;
 				radio312.addEventListener('click', async (e: Event) =>{
                 });
-
+				divCont1.appendChild(divCont2);
             });
+			
             let radio32 = document.getElementById('radio32') as HTMLInputElement;
             radio32.addEventListener('click', async (e: Event) =>{	
 
             });
+
             divCont.appendChild(divCont1);
+		}
     } 
-}
