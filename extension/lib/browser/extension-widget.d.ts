0152346fac67896b336876d435a3334c7ae93ab0
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-27 00:12:43 +0200

    Adding missing copyrights
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- extension/lib/browser/extension-widget.d.ts
+++ extension/lib/browser/extension-widget.d.ts
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (C) 2021-2022 University of Macedonia
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 import * as React from 'react';
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
 import { MessageService } from '@theia/core';
